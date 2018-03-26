35789511e3b456920f5cfe3f733766a18943101b
hwang763 <35672336+hwang763@users.noreply.github.com>
2018-03-25 21:24:58 -0400

    Initial

---
+++ ckim328_SE2250_ProjectPart1-master/Phase1_ckim328/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	[Header("Set in Inspector: Enemy")]
+	public float speed=10f;
+	public float fireRate = 0.3f;
+	public float health= 10;
+	public int score=100; 
+
+	private BoundsCheck bndCheck;
+
+	void Awake(){
+		bndCheck = GetComponent<BoundsCheck> ();
+	}
+	//this is a property, a method that acts like a field
+	public Vector3 pos {
+		get {
+			return(this.transform.position);
+		}
+		set{ this.transform.position = value; }
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame  
+	void Update () {
+		Move();
+
+		if (bndCheck!= null&& bndCheck.offDown){
+			// we have gone off the bottom of the screen so we need to destroy the object 
+			Destroy (gameObject);
+		}
+
+	}
+	public virtual void Move(){
+		Vector3 tempPos = pos;
+		tempPos.y -= speed * Time.deltaTime;
+		pos = tempPos;
+	}
+	void OnCollisionEnter(Collision coll){
+		GameObject	otherGO = coll.gameObject;
+		if (otherGO.tag == "ProjectileHero") {
+			Destroy (otherGO);
+			Destroy (gameObject);
+
+		} else {
+			print ("Enemy hit by non-projectilehero" + otherGO.name);
+		}
+	}
+}
---
+++ ckim328_SE2250_ProjectPart1-master/Phase1_ckim328/Assets/__Scripts/Weapon.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Weapon : MonoBehaviour {
+	static public Transform PROJECTILE_ANCHOR;
+
+	[Header ("Set Dynamically")] [SerializeField]
+	private WeaponType _type = WeaponType.none;
+	public WeaponDefinition def;
+	public GameObject collar;
+	public float lastShotTime;
+	private Renderer collarRend;
+		
+
+	// Use this for initialization
+	void Start () {
+		collar = transform.Find ("Collar").gameObject;
+		collarRend = collar.GetComponent<Renderer>();
+
+		SetType (_type);
+
+
+		if (PROJECTILE_ANCHOR == null) {
+			GameObject go = new GameObject ("_ProjectileAnchor");
+			PROJECTILE_ANCHOR = go.transform;
+		}
+
+		GameObject rootGO = transform.root.gameObject;
+		if (rootGO.GetComponent<Hero> () != null) {
+			rootGO.GetComponent<Hero> ().fireDelegate += Fire;
+		
+		}
+	}
+	public WeaponType type {
+		get { return (_type); }
+		set { SetType (value); }
+	}
+
+	public void SetType(WeaponType wt){
+		_type=wt;
+		if (_type==WeaponType.none){
+			this.gameObject.SetActive(false);
+			return;
+		} else{
+			this.gameObject.SetActive (true);
+		}
+		def = Main.GetWeaponDefinition (_type);
+		collarRend.material.color = def.color;
+		lastShotTime = 0;
+	}
+
+	public void Fire (){
+		if (!gameObject.activeInHierarchy)
+			return;
+		if (Time.time - lastShotTime < def.delayBetweenShots) {
+			return;
+		}
+
+		Projectile p;
+		Vector3 vel = Vector3.up * def.velocity;
+		if (transform.up.y < 0) {
+			vel.y = -vel.y;
+		}
+		switch (type) {
+		case  WeaponType.blaster:
+			p = MakeProjectile ();
+			p.rigid.velocity = vel;
+			break;
+		
+		case WeaponType.spread:
+			p = MakeProjectile ();//makes the middle projectile
+			p.rigid.velocity = vel;
+			p = MakeProjectile ();//makes right projectile
+			p.transform.rotation = Quaternion.AngleAxis (30, Vector3.forward);
+			p.rigid.velocity = p.transform.rotation * vel;
+			p = MakeProjectile ();//makes left projectile
+			p.transform.rotation = Quaternion.AngleAxis (30, Vector3.back);
+			p.rigid.velocity = p.transform.rotation * vel;
+			break;
+		}
+	}
+
+
+	public Projectile MakeProjectile(){
+		GameObject go = Instantiate<GameObject>(def.projectilePrefab);
+		if (transform.parent.gameObject.tag=="Hero"){
+			go.tag="ProjectileHero";
+			go.layer=LayerMask.NameToLayer("ProjectileHero");
+		} else {
+		//	go.tag= "ProjectileEnemy" ;
+		go.layer= LayerMask.NameToLayer("ProjectileEnemy");
+		}
+		go.transform.position=collar.transform.position;
+		go.transform.SetParent(PROJECTILE_ANCHOR, true);
+		Projectile p = go.GetComponent<Projectile>();
+		p.type=type;
+		lastShotTime= Time.time;
+		return (p);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			SetType (WeaponType.blaster);
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			SetType (WeaponType.spread);
+		}
+	}
+}
